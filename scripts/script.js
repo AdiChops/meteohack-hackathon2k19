3a716774e3e9580adf5a05eb96534c1529fd9901
Chopra <Aaditya.Chopra@ec.gc.ca>
2019-07-09 14:06:08 -0400

    Separated script from page

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+let shuffle = function (myArray) {
+    for (let i = myArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = myArray[j];
+        myArray[j] = myArray[i];
+        myArray[i] = temp;
+    }
+    return myArray;
+};
+fetch("sample_questions.json")
+    .then(data => {
+        return data.json();
+    })
+    .then(data => {
+        console.log(data);
+        let questions = shuffle(data["questions"]);
+        for (let i of questions) {
+            if (i["question_type"] == "M") {
+                let answers = i["answers"];
+                console.log(answers);
+                answers = shuffle(answers);
+                console.log(answers);
+            }
+        }
+    });
