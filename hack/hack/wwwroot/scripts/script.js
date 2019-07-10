5ed313b2093690f663b99dcac01ec28277daf3cd
Skranbets <tianweiding3@gmail.com>
2019-07-10 17:39:02 -0400

    fhg

--- hack/hack/wwwroot/scripts/script.js
+++ hack/hack/wwwroot/scripts/script.js
@@ -4,30 +4,55 @@ let num = 0;
 function customAlert(msg,callback) {
     // code to show your custom alert
     // in this case its just a console log
-    console.log(num);
-    $('.choices').click(function () {
-        console.log(num)
-        if ($(this).text() ==msg[num]["correct"]){
-            document.querySelector("#question").innerHTML = `<p>Correct!</p>`;
-        }
-        else{
-            document.querySelector("#question").innerHTML = `<p>Incorrect!</p>`;
-        }
-        $("#visual").addClass('animated');
-        $("#visual").addClass('flip');
-        $("#visual").css("padding-bottom", "inherit");
-        $("#visual").css("background-image", "none");
-        $("#answer1").show();
-        $("#multiAnswer").hide();
-        $("#next").show()
-       
-        $("#btnNext").click(function(){
-            $('.choices').off("click")
-            $('#btnNext').off("click")
-            callback(msg);
-        })
-       
-      });
+    if(msg[num]["question_type"] != "S"){
+        $('.choices').click(function () {
+            console.log(num)
+            if ($(this).text() ==msg[num-1]["correct"]){
+                document.querySelector("#question").innerHTML = `<p>Correct!</p>`;
+            }
+            else{
+                document.querySelector("#question").innerHTML = `<p>Incorrect!</p>`;
+            }
+            $("#visual").addClass('animated');
+            $("#visual").addClass('flip');
+            $("#visual").css("padding-bottom", "inherit");
+            $("#visual").css("background-image", "none");
+            $("#answer1").show();
+            $(".answer").hide();
+            $("#next").show()
+           
+            $("#btnNext").click(function(){
+                $('.choices').off("click")
+                $('#btnNext').off("click")
+                callback(msg);
+            })
+           
+          });
+    }
+    else{
+        $('#submit').click(function () {
+            if ($("#ans").val().toLowerCase() ==msg[num-1]["correct"].toString().toLowerCase()){
+                document.querySelector("#question").innerHTML = `<p>Correct!</p>`;
+            }
+            else{
+                document.querySelector("#question").innerHTML = `<p>Incorrect!</p>`;
+            }
+            $("#visual").addClass('animated');
+            $("#visual").addClass('flip');
+            $("#visual").css("padding-bottom", "inherit");
+            $("#visual").css("background-image", "none");
+            $("#answer1").show();
+            $(".answer").hide();
+            $("#next").show()
+           
+            $("#btnNext").click(function(){
+                $('.choices').off("click")
+                $('#btnNext').off("click")
+                callback(msg);
+            })
+        });
+    }
+    
 
     // do callback when ready
     
